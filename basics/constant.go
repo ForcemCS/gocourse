7e9854fd802a57747bd1d7846e49c487845ff09b
ForcemCS <kuiinative@gmail.com>
2025-05-03 16:07:49 +0800

    const的使用

---
+++ basics/constant.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const pi = 3.14
+
+const MONDAY = 1
+
+func main() {
+
+	const (
+		tuesday  = 2
+		wenesday = 3
+	)
+
+	fmt.Println(pi, MONDAY, tuesday, wenesday)
+
+}
